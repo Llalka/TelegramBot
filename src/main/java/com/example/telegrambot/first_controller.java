f09c60d066685d6a57dd857a9f61fba1ad692a17
Lalka <arsenyshestakk@gmail.com>
2023-02-20 16:55:55 +0300

    initial commit

---
+++ src/main/java/com/example/telegrambot/first_controller.java
@@ -0,0 +1,16 @@
+package com.example.telegrambot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class first_controller {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        return "home";
+    }
+
+}
